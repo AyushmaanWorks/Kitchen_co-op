f425b883c653fa2fb7cf2e968d6873194a5c2a73
AyushmaanWorks <186050806+AyushmaanWorks@users.noreply.github.com>
2025-05-22 15:18:23 +0530

    Home-page-init

--- we_cooked_that/src/app/page.tsx
+++ we_cooked_that/src/app/page.tsx
@@ -1,11 +1,18 @@
+'use client'
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Fredoka } from "next/font/google";
+const fredoka = Fredoka({ subsets: ["latin"], weight: ["400", "700"] });
+
 
 export default function Home() {
   return (
     <div className="bg-cover bg-center min-h-screen min-w-screen bg-[url('/images/homebg.png')]">
-      <div className="bg-transparent min-h-screen max-w-[40vw] flex flex-col items-cetner justify-center">
-        hello
+      <div className="bg-transparent min-h-screen max-w-[28vw] flex flex-col items-center justify-center">
+        <Button className={` rounded-2xl font-extrabold min-w-[20vw] text-[#FFDBAF] text-4xl hover:text-5xl hover:cursor-pointer transition-all duration-300 ${fredoka.className}`}>
+          Start Cooking...
+        </Button>
       </div>
     </div>
-  )
+  );
 }
