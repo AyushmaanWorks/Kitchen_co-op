c82754f4cedb0c881793b50da75343f0736b5c59
AyushmaanWorks <186050806+AyushmaanWorks@users.noreply.github.com>
2025-05-22 13:53:01 +0530

    Initial commit

---
+++ we_cooked_that/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-cover bg-center min-h-screen min-w-screen bg-[url('/images/homebg.png')]">
+      <div className="bg-transparent min-h-screen max-w-[40vw] flex flex-col items-cetner justify-center">
+        hello
+      </div>
+    </div>
+  )
+}
