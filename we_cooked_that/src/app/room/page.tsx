b8da15ddcc964fc3b9929155dd8f37bbfdfa60bf
AyushmaanWorks <186050806+AyushmaanWorks@users.noreply.github.com>
2025-05-23 22:40:41 +0530

    create room done

--- we_cooked_that/src/app/room/page.tsx
+++ we_cooked_that/src/app/room/page.tsx
@@ -8,7 +8,10 @@ import { useState } from "react";
 export default function Home() {
   const [name, setName] = useState("");
   const [step, setStep] = useState("name");
-  const handleCreateRoom = () => {setStep("create")};
+  const [roomId, setRoomId] = useState("0210");
+  const handleCreateRoom = () => {
+    setStep("create");
+  };
 
   return (
     <div className="h-screen w-screen bg-cover bg-center bg-[url('/images/room.png')] overflow-hidden flex flex-col">
@@ -28,22 +31,22 @@ export default function Home() {
         <Image src="/images/logo.pn g" alt="logo" width={150} height={150} />
       </div>
 
-      <div className="h-[80%] min-w-full flex items-center justify-center gap-8 flex-col">
-        <div>
-          <div className="flex item-center justify-center px-4 bg-[#FFFFFF]/70 rounded-full p-2">
-            <input
-              type="text"
-              placeholder="Enter your name..."
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              className={`text-3xl text-[#D52A78] font-extrabold glow-pink ${fredoka.className} px-4 py-2 rounded-md outline-none placeholder-pink-[#D52A78] bg-transparent`}
-            />
+      {/* ENTER NAME */}
+
+      {step === "name" && (
+        <div className="h-[80%] min-w-full flex items-center justify-center gap-8 flex-col">
+          <div>
+            <div className="flex item-center justify-center px-4 bg-[#FFFFFF]/70 rounded-full p-2">
+              <input
+                type="text"
+                placeholder="Enter your name..."
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className={`text-3xl text-[#D52A78] font-extrabold glow-pink ${fredoka.className} px-4 py-2 rounded-md outline-none placeholder-pink-[#D52A78] bg-transparent`}
+              />
+            </div>
           </div>
-        </div>
 
-        {/* ENTER NAME */}
-
-        {step === "name" && (
           <div className="flex flex-row gap-8">
             <Button
               className={`relative w-[280px] h-[118px] rounded-2xl font-extrabold text-[#fff7e4] text-3xl transition-all duration-300 hover:scale-105 hover:cursor-pointer ${fredoka.className}`}
@@ -78,10 +81,53 @@ export default function Home() {
               </div>
             </Button>
           </div>
-        )}
+        </div>
+      )}
 
-        {/* ENTER ROOM */}
-      </div>
+      {step === "create" && (
+        <div className="h-full w-full flex flex-col">
+          {/* Middle Bar - Room ID */}
+          <div className="h-[20%] flex items-center justify-center">
+            <div
+              className={`relative w-[300px] h-[118px] rounded-2xl font-extrabold text-[#fff7e4] text-3xl ${fredoka.className}`}
+              onClick={handleCreateRoom}
+            >
+              <Image
+                src="/images/custombutton.png"
+                alt="Create Room"
+                layout="fill"
+                objectFit="contain"
+              />
+              <div className="absolute inset-0 flex items-center justify-center">
+                <p className="text-white -translate-y-1 text-4xl font-bold">
+                  Room ID: {roomId}
+                </p>
+              </div>
+            </div>
+          </div>
+
+          {/* Bottom Section */}
+          <div className="h-[60%] w-full relative">
+            <div className="pl-40 mt-20">
+              <Image
+                src="/images/pinkcat.png"
+                alt="Chef Cat"
+                width={280}
+                height={280}
+              />
+            </div>
+            <div className="absolute bottom-[15%] right-8">
+              <div className="bg-white/70 rounded-full px-10 py-3 mr-30 mb-15">
+                <h1
+                  className={`text-3xl text-[#4B2C61] font-extrabold drop-shadow-[0_3px_2px_rgba(0,0,0,0.3)] ${fredoka.className}`}
+                >
+                  waiting for Player 2
+                </h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
